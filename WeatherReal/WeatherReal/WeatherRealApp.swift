d78d6c6edd7d56ac6ac2254d453a12feba3c9164
bryangomezcarranza <bryangomezcarranza@gmail.com>
2024-01-25 13:54:23 -0800

    Creates Xcode project

---
+++ WeatherReal/WeatherReal/WeatherRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherRealApp.swift
+//  WeatherReal
+//
+//  Created by Bryan Gomez on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
