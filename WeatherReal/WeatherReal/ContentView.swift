96fca0435b26f3579bdac8e288a795332ed41219
bryangomezcarranza <bryangomezcarranza@gmail.com>
2024-01-25 17:24:22 -0800

    initial design

--- WeatherReal/WeatherReal/ContentView.swift
+++ WeatherReal/WeatherReal/ContentView.swift
@@ -13,10 +13,49 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Seattle")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+            
+            Text("Sep 19 1996")
+            HStack(alignment: .top) {
+                Text("60")
+                    .font(.system(size: 160, weight: .regular))
+                Image(systemName: "circle")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .bold()
+            }
+            
+            // Details
+            HStack(spacing: 40){
+                VStack {
+                    Image(systemName: "wind")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Windy")
+                }
+               
+                VStack {
+                    Image(systemName: "wind")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Windy")
+                }
+                
+                VStack {
+                    Image(systemName: "wind")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Windy")
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.all, 32)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color.black)
+            )
         }
         .onAppear(perform: {
             locationManager.requestLocationPermission()
